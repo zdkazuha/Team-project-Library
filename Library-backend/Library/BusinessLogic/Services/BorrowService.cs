397c6a262f6f9d4b41705bd458373f6a632073ec
Artem <aartemchelyk@gmail.com>
2025-11-27 17:53:41 +0200

    Refactor borrow check logic for clarity

--- Library-backend/Library/BusinessLogic/Services/BorrowService.cs
+++ Library-backend/Library/BusinessLogic/Services/BorrowService.cs
@@ -83,10 +83,10 @@ namespace BusinessLogic.Services
                 includes: new[] { nameof(Borrow.Book) }
             );
 
-            if (!borrows.Any())
+            if (borrows.Count() == 0)
                 return false;
 
-            var borrow = borrows.First();
+            var borrow = borrows[0];
 
             if (borrow.ReturnedAt != null)
                 throw new Exception("Book already returned.");
